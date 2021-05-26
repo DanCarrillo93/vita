22178b269519495244d159d8146b3d0fbbf20cf6
DanCarrillo93 <64762408+DanCarrillo93@users.noreply.github.com>
2021-05-26 14:42:05 -0700

    Fixed AK47 seeder

--- seeder/seed.js
+++ seeder/seed.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const db = require("../models");
+require("dotenv").config();
+
+console.log(process.env.MONGODB_URI);
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
