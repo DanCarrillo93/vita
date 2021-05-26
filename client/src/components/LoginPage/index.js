692f801458f724b4ad797b5e9f3bf568e6504867
Oliver Shih <olivershih@gmail.com>
2021-05-26 11:13:53 -0700

    font russo one hooked up

--- client/src/components/LoginPage/index.js
+++ client/src/components/LoginPage/index.js
@@ -26,11 +26,11 @@ function LoginPage() {
     : auth.error || "Please complete the form and click submit.";
 
   return (
-    <div className="container mx-auto px-4 bg-gray-700 rounded-md p-5">
-      <h1 className="text-gray-200 text-center text-7xl font-medium border-b-4 pb-10">Login</h1>
+    <div className="container mx-auto px-4 bg-gray-700 rounded-md p-5 w-3/4">
+      <h1 className="text-gray-200 text-center text-7xl font-medium border-b-4 pb-10 font-russo">Login</h1>
       <form onSubmit={handleSubmit}>
         <div className="flex flex-col">
-          <label htmlFor="email" className="text-gray-200 text-4xl font-medium mx-12 my-3">Email</label>
+          <label htmlFor="email" className="text-gray-200 text-4xl font-medium mx-12 my-3 font-russo">Email</label>
           <input
             id="email"
             type="email"
@@ -41,7 +41,7 @@ function LoginPage() {
           />
         </div>
         <div className="flex flex-col">
-          <label htmlFor="password" className="text-gray-200 text-4xl font-medium mx-12 my-3">Password</label>
+          <label htmlFor="password" className="text-gray-200 text-4xl font-medium mx-12 my-3 font-russo">Password</label>
           <input
             id="password"
             type="password"
@@ -53,14 +53,14 @@ function LoginPage() {
         </div>
         <br />
         <div className="flex flex-row-reverse">
-          <button type="submit" className="text-gray-700 text-4xl font-medium bg-gray-200 rounded-lg px-3 py-1 mx-12" disabled={auth.pending}>
+          <button type="submit" className="text-gray-700 text-4xl font-medium bg-gray-200 rounded-lg px-3 py-1 mx-12 font-russo" disabled={auth.pending}>
             {auth.pending ? "⌛" : "Submit"}
           </button>
         </div>
+        <div className="flex flex-row-reverse">
+          <p className="mx-12 text-xl text-gray-200 mt-2">{message}</p>
+        </div>
       </form>
-      <div className="flex flex-row-reverse">
-        <p className="mx-12 text-xl text-gray-200 mt-2">{message}</p>
-      </div>
     </div>
   );
 }
