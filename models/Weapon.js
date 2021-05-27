fd40496d9c16a7616c9bc5dbe669fab854d11b08
zax5021 <zacharyacampbell1@gmail.com>
2021-05-27 15:16:44 -0700

    updated add Item to take name

--- models/Weapon.js
+++ models/Weapon.js
@@ -1,40 +1,41 @@
 const mongoose = require("mongoose");
 
 const WeaponSchema = new mongoose.Schema({
-    id: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    URL: {
-        type: String,
-        required: true,
-    },
-    weapon_type: {
-        type: String,
-        required: true
-    },
-    sub_type: {
-        type: String,
-        required: true
-    },
-    rarity: {
-        type: String,
-        required: true
-    },
-    skin: {
-        type: String,
-        required: true,
-    },
-    condition: {
-        type: String,
-        required: true
-    }
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+  },
+  URL: {
+    type: String,
+    required: true,
+  },
+  weapon_type: {
+    type: String,
+    required: true,
+  },
+  sub_type: {
+    type: String,
+    required: true,
+  },
+  rarity: {
+    type: String,
+    required: true,
+  },
+  skin: {
+    type: String,
+    required: true,
+  },
+  condition: {
+    type: String,
+    required: true,
+  },
 });
 
 const Weapon = mongoose.model("Weapon", WeaponSchema);
