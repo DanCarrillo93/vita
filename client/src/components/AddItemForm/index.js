03c0e6404b188f0c6833e564524f7da55dcdb064
Oliver Shih <olivershih@gmail.com>
2021-05-27 14:50:14 -0700

    almsot done with add weapon form

---
+++ client/src/components/AddItemForm/index.js
@@ -0,0 +1,41 @@
+function AddItemForm({weaponList, handleConditionChange, handleWeaponChange, skinList, handleSkinChange}) {
+    return (
+        <form className="border-4 border-gray-300 rounded p-3">
+            Add Item
+            <div className="font-russo text-base grid grid-cols-2">
+                <label htmlFor="weapon-type" className="text-right my-2 pr-2">Weapon Type</label>
+                <select onChange={handleWeaponChange} id="weapon-type" name="Weapon Type" className="text-gray-900 my-2">
+                <option key="aha" value="aha">Pick a weapon</option>
+                    {weaponList.map(weapon => {
+                        return (
+                            <option key={weapon.weapon} value={weapon.weapon}>{weapon.weapon}</option>
+                        )
+                    })}
+                </select>
+                <label htmlFor="skin-type" className="text-right my-2 pr-2">Skin Type</label>
+                <select onChange={handleSkinChange} id="skin-type" name="Skin Type" className="text-gray-900 my-2">
+                    <option key="aha" value="aha">Pick a skin</option>
+                    {skinList.map(skin => {
+                        return (
+                            <option key={skin} value={skin}>{skin}</option>
+                        )
+                    })}
+                </select>
+                <label htmlFor="condition" className="text-right my-2 pr-2">Condition</label>
+                <select onChange={handleConditionChange} id="condition" name="Condition" className="text-gray-900 my-2">
+                    <option key="aha" value="aha">Pick a condition</option>
+                    <option value="Factory New">Factory New</option>
+                    <option value="Minimal Wear">Minimal Wear</option>
+                    <option value="Field-Tested">Field-Tested</option>
+                    <option value="Well-Worn">Well-Worn</option>
+                    <option value="Battle-Scarred">Battle-Scarred</option>
+                </select>
+            </div>
+            <div className="flex flex-row justify-end">
+                <button type="submit" className="text-sm mt-2 py-2 px-3 font-sans font-bold border-2 border-green-900 bg-green-700 rounded">Add an item</button>
+            </div>
+        </form>
+    )
+}
+
+export default AddItemForm;
--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,18 +1,61 @@
 import { useAuth } from "../../util/auth";
 import SimpleCard from "../SimpleCard";
+import AddItemForm from "../AddItemForm";
+import { useState } from "react";
+const weaponList = require("../../data/weapons.json");
+
 // PrivatePage is an example include to demonstrate a route protected from
 // unauthenticated users. See the routing in App.js.
 function Dashboard() {
   const auth = useAuth();
+
+  const [formWeapon, setFormWeapon] = useState("");
+  const [formSkin, setFormSkin] = useState("");
+  const [formCondition, setFormCondition] = useState("");
+  const [skinList, setSkinList] = useState([]);
+
+  async function handleConditionChange(e) {
+    e.preventDefault();
+    const condition = e.target.value;
+    if (condition === "aha") {
+      setFormCondition("");
+      return;
+    };
+    setFormCondition(condition);
+  }
+
+  async function handleSkinChange(e) {
+    e.preventDefault();
+    const skin = e.target.value;
+    if (skin === "aha") {
+      setFormSkin("");
+      return;
+    };
+    setFormSkin(skin);
+  }
+
+  async function handleWeaponChange(e) {
+    e.preventDefault();
+    const weapon = e.target.value;
+    if (weapon === "aha") {
+      setSkinList([]);
+      setFormSkin("");
+      return;
+    };
+    const skinList = require(`../../data/skins/${weapon}-skinList.json`);
+    await setFormWeapon(weapon);
+    await setSkinList(skinList);
+    }
+
   return (
     <div className="container mx-auto font-russo">
-      <h1 className="text-5xl text-gray-300 flex-root border-4 border-gray-300 rounded p-1 mx-1 mt-2 pb-1">
+      <h1 className="text-5xl text-gray-300 flex-root border-4 border-gray-300 rounded p-3 mx-1 mt-2 pb-1">
         Hello, {auth.user.username}!
       </h1>
 
       <div className="text-gray-200 flex flex-row mt-1">
         <div className="w-1/4 border-4 border-gray-300 rounded p-2 mx-1 my-2 text-3xl">
-          Forms
+          <AddItemForm weaponList={weaponList} skinList={skinList} handleConditionChange={handleConditionChange} handleSkinChange={handleSkinChange} handleWeaponChange={handleWeaponChange} />
         </div>
         <div className="w-1/2 border-4 border-gray-300 rounded p-2 mx-1 my-2 gap-4 text-3xl">
           Inventory
