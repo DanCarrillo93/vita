22303d41897ba3eccfda4ea22c6d84bf412a40de
zax5021 <zacharyacampbell1@gmail.com>
2021-05-26 22:00:08 -0700

    updated the bundle and user models to ref weapons instead of copy

--- models/User.js
+++ models/User.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 const { hash, compare } = require("bcrypt");
 const { isEmail } = require("../util/validate");
-const uuid = require("mongoose-uuid");
+const uuid = require("node-uuid");
+const { Schema } = mongoose;
 
 const HASH_SALT_ROUNDS = 10;
 
 // Add additional fields to the user schema as needed for your app. Feel free to
 // modify username, email, and password validations. email as part of the login
 // credentials.
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
   username: {
     type: String,
     required: true,
@@ -41,8 +42,13 @@ const UserSchema = new mongoose.Schema({
     itemId: {
       type: String,
       default: uuid.v1,
-      unique: true},
-      skinId: String, sub_type: String, skin: String, condition: String, URL: String, rarity: String
+      unique: true
+    },
+    item: {
+      type: String,
+      ref: 'Weapon'
+    }
+      // skinId: String, sub_type: String, skin: String, condition: String, URL: String, rarity: String
   }]
 });
 
