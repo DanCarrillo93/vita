0009ffcc9e406b3d7d8c6d12a0eb9846ff4471f5
amata7 <adrianmata@live.com>
2021-05-27 10:27:12 -0700

    template cards displaying on homepage and dashboard

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,13 +1,19 @@
 import { useAuth } from "../../util/auth";
-import SimpleCard from "../SimpleCard";
+import DetailedCard from "../DetailedCard";
 // PrivatePage is an example include to demonstrate a route protected from
 // unauthenticated users. See the routing in App.js.
 function Dashboard() {
   const auth = useAuth();
   return (
-    <div>
-      <h1 className="text-5xl text-gray-200">Hello, {auth.user.username}!</h1>;
-      <SimpleCard />
+    <div className="container mx-auto">
+      <h1 className="text-5xl text-gray-200 flex-root">
+        Hello, {auth.user.username}!
+      </h1>
+      ;
+      <div className="container mx-auto flex">
+        <DetailedCard />
+        <DetailedCard />
+      </div>
     </div>
   );
 }
