fb92e96bbf48f39e003f624c8b01d6cae5393666
Oliver Shih <olivershih@gmail.com>
2021-05-31 14:59:36 -0700

    added login conditional to disable buy bundle button

--- client/src/components/ListingPage/index.js
+++ client/src/components/ListingPage/index.js
@@ -1,7 +1,16 @@
 import { useParams } from "react-router-dom";
+import { useAuth } from "../../util/auth";
 
 function ListingPage() {
     const { id } = useParams();
+    const auth = useAuth();
+    let color;
+    if (auth.isLoggedIn()) {
+        color = "green";
+    } else {
+        color = "gray";
+    }
+
     return (
         <>
             <div className="text-5xl text-gray-200">
@@ -13,7 +22,7 @@ function ListingPage() {
                         4 items listed for $50
                     </h6>
                 </div>
-                <button type="submit" className="mx-3 py-3 px-4 font-sans font-bold border-2 border-green-900 bg-green-700 rounded">Buy bundle</button>
+                <button type="submit" className={`mx-3 py-3 px-4 font-sans font-bold border-2 border-${color}-900 bg-${color}-700 rounded`} disabled={!auth.isLoggedIn()}>Buy bundle</button>
             </form>
         </>
     )
