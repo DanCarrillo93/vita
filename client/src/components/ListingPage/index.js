6ba0134daf27b4b698558094f135c51b2d6912b0
Oliver Shih <olivershih@gmail.com>
2021-05-31 14:53:13 -0700

    added placeholder message and button to listing page

--- client/src/components/ListingPage/index.js
+++ client/src/components/ListingPage/index.js
@@ -3,9 +3,19 @@ import { useParams } from "react-router-dom";
 function ListingPage() {
     const { id } = useParams();
     return (
-        <div className="text-5xl text-gray-200">
-            Listing Page: {id}
-        </div>
+        <>
+            <div className="text-5xl text-gray-200">
+                Listing Page: {id}
+            </div>
+            <form className="flex flex-row justify-end text-gray-200 text-2xl">
+                <div className="flex flex-col justify-center">
+                    <h6>
+                        4 items listed for $50
+                    </h6>
+                </div>
+                <button type="submit" className="mx-3 py-3 px-4 font-sans font-bold border-2 border-green-900 bg-green-700 rounded">Buy bundle</button>
+            </form>
+        </>
     )
 }
 
